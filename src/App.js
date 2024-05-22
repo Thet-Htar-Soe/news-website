52300dcfeb4d97385fa728f8eae4d6ed74c506b9
ThetHtarSoe <thethtarsoe2761@gmail.com>
2024-05-22 18:01:07 +0800

    [master] design for travel page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import NavigationBar from "./components/Navbar";
 import HeaderPage from "./pages/HeaderPage";
 import TechnologyPage from "./pages/TechnologyPage";
+import TravelPage from "./pages/TravelPage";
 import { fetchLatestArticles } from "./Api";
 import "./custom.css";
 
@@ -23,8 +24,9 @@ const App = () => {
 
   return (
     <div>
+
       {/* Header Page Start */}
-      <div style={{ height: "100vh" }}>
+      <div style={{}}>
 
         <div className="h-100 d-flex flex-column">
           <NavigationBar />
@@ -43,6 +45,16 @@ const App = () => {
         </div>
       </div>
       {/* Technology Page End */}
+
+      {/* Travel Page Start */}
+      <div className="bg-fixed">
+
+        <div className="custom-border-top">
+
+          <TravelPage travelnews={latestNews} />
+        </div>
+      </div>
+      {/* Travel Page End */}
 
     </div>
   )
---
+++ src/pages/TravelPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import NewsCard from "../components/NewsCard";
+
+const TravelPage = ({ travelnews }) => {
+    return (
+        <Container fluid>
+            <h4 className="mx-3 text-white py-3">Travel</h4>
+            <Row className="mx-3 pt-2">
+                <Col lg={6} md={6} sm={12}>
+                    <h1 className="custom-travel-text">Roam<br /><span className="custom-travel-earth">The earth</span></h1>
+                    <p className="w-75 text-justify d-inline-block">Globetrotting. This is a word used to describe someone who's going on a long journey around the world, and a globetrotter is someone who travels a lot, and all over the world, especially for sightseeing.</p>
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                    {travelnews.map((article, index) => (
+                        <NewsCard key={index} article={article} customStyle="custom-travel-card" lineClamp={1} />
+                    ))}
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default TravelPage;
